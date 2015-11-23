24f3a35c1718ef3a2121252e06908b07ee815e03
BrotherSeamus <jamespkirwin@gmail.com>
2015-11-23 08:49:39 -0600

    added comments to javascript

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -6,22 +6,23 @@ $(function () {
 	var templateCurrent = Handlebars.compile($('#current').html());
 	var templateNext = Handlebars.compile($('#next').html());
 
+	/*Uses Ajax to feed data into Handlebars templates with fade animations*/
 	function render () {
 		$.ajax({
 			url:"/data/eta.json"
 		}).done(function(array){
 			$('.previous').fadeOut(500, function(){
-				$(this).html(templatePrevious(array[count-1]));
+				$(this).html(templatePrevious(array[count-1]));//renders the previous button
 			}).fadeIn(500);
 			$('.next').fadeOut(500, function(){
-				$(this).html(templateNext(array[count+1]));
+				$(this).html(templateNext(array[count+1]));//renders the next button
 			}).fadeIn(500);
 			$('.current').fadeOut(500, function(){
-				$(this).html(templateCurrent(array[count]));
+				$(this).html(templateCurrent(array[count]));//renders the current info section
 			}).fadeIn(500);
 		});
 	};
-
+	/*Advances through the carousel one step*/
 	function advance (){
 		count++;
 		if (count==21) {
@@ -31,9 +32,9 @@ $(function () {
 			render();
 		};
 	};
-
+	/*calls the render function*/
 	render();
-
+	/*Event handler for the previous button also resets the setInterval*/
 	$('.previous').on('click', function() {
 		count--;
 		if (count<0){
@@ -47,7 +48,7 @@ $(function () {
 			advance();
 		}, 10000)
 	});	
-
+	/*Event handler for the next button also resets the setInterval*/
 	$('.next').on('click', function() {
 		advance();
 		clearInterval(slideShow);
@@ -55,7 +56,7 @@ $(function () {
 			advance();
 		}, 10000)
 	});	
-
+	/*Sets the interval for carousel of student data*/
 	var slideShow = setInterval(function(){
 		advance();
 	}, 10000)
